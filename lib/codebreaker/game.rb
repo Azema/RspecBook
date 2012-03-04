3b2ab59f6bb45daa64cc48721e9b0c7a8e0ce943
Manuel HERVO <manuel.hervo@gmail.com>
2012-03-04 22:08:57 +0100

    Initial import, page 78

---
+++ lib/codebreaker/game.rb
@@ -0,0 +1,13 @@
+module Codebreaker
+  class Game
+    def initialize(output)
+      @output = output
+    end
+
+    def start
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
+    end
+  end
+end
+
